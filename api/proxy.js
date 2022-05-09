04ababa2205415df3a718fd475d59ad89e7956d6
Evan You <yyx990803@gmail.com>
2022-05-09 14:10:49 +0800

    proxy

---
+++ api/proxy.js
@@ -0,0 +1,17 @@
+// 该服务为 vercel serve跨域处理
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = (req, res) => {
+  let target = "";
+  if (req.url.startsWith("/amis")) {
+    target = "https://aisuda.bce.baidu.com";
+  }
+  // 创建代理对象并转发请求
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+    pathRewrite: {
+      "^/amis/": "/",
+    },
+  })(req, res);
+};
