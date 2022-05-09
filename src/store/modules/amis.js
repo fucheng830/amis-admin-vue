b00268ebd7a18e0821b94baeecff3f827284ca0a
Evan You <yyx990803@gmail.com>
2022-05-09 18:08:14 +0800

    feat: icon

--- src/store/modules/amis.js
+++ src/store/modules/amis.js
@@ -1,15 +1,23 @@
 const state = () => ({
   config: {},
+  enableAMISDebug: false,
 });
 const getters = {
   config: (state) => state.config,
+  enableAMISDebug: (state) => state.enableAMISDebug,
 };
 const mutations = {
   setProtocol(state, config) {
     state.config = config;
   },
+  enableAMISDebug(state, enable) {
+    state.enableAMISDebug = enable;
+  },
 };
 const actions = {
+  enableAMISDebug({ commit }, enable) {
+    commit("enableAMISDebug", enable);
+  },
   setProtocol({ commit }, config) {
     commit("config", config);
   },
