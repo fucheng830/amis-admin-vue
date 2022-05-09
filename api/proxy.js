4aaaba9811f5003dcd54f547e1fd4c2dc78a2dae
Evan You <yyx990803@gmail.com>
2022-05-09 14:23:48 +0800

    proxy

--- api/proxy.js
+++ api/proxy.js
@@ -4,7 +4,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = (req, res) => {
   let target = "";
   if (req.url.startsWith("/amis")) {
-    target = "https://aisuda.bce.baidu.com";
+    target = "https://aisuda.bce.baidu.com/amis/";
   }
   // 创建代理对象并转发请求
   createProxyMiddleware({
