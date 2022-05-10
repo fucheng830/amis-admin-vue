23f3513d89ce3a2edf5e4821b1dc6b482090eaed
h7ml <h7ml@qq.com>
2022-05-10 09:55:40 +0800

    ✨特性(view): ✨特性(view): vercel cache
    vercel 缓存

--- api/proxy.js
+++ api/proxy.js
@@ -1,17 +1,18 @@
 // 该服务为 vercel serve跨域处理
-const { createProxyMiddleware } = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
 module.exports = (req, res) => {
-  let target = "";
-  if (req.url.startsWith("/amis")) {
-    target = "https://aisuda.bce.baidu.com/amis/";
+  let target = ''
+  if (req.url.startsWith('/amis')) {
+    target = 'https://aisuda.bce.baidu.com/amis/'
   }
+  res.set('Cache-Control', 'Cache-Control: s-maxage=9999')
   // 创建代理对象并转发请求
   createProxyMiddleware({
     target,
     changeOrigin: true,
     pathRewrite: {
-      "^/amis/": "/",
-    },
-  })(req, res);
-};
+      '^/amis/': '/'
+    }
+  })(req, res)
+}
