d7751c78ffd79bfc3884ae63163bc49b58bdb3fe
h7ml <h7ml@qq.com>
2022-05-10 08:36:05 +0800

    🐛修复(style): 修复页面切换样式丢失
    设置shamefully-hoist 为默认值

--- vue.config.js
+++ vue.config.js
@@ -38,27 +38,27 @@ module.exports = {
     }
   },
   chainWebpack(config) {
-    const rule = config.module.rule('js')
+    // const rule = config.module.rule('js')
 
-    // 清理自带的 babel-loader
-    rule.uses.clear()
-
-    // 添加 esbuild-loader
-    rule
-      .use('esbuild-loader')
-      .loader('esbuild-loader')
-      .options({
-        loader: 'ts', // 如果使用了 ts, 或者 vue 的 class 装饰器，则需要加上这个 option 配置， 否则会报错：ERROR: Unexpected "@"
-        target: 'es2015',
-        tsconfigRaw: require('./tsconfig.json')
-      })
-    // 删除底层 terser, 换用 esbuild-minimize-plugin
-    config.optimization.minimizers.delete('terser')
-
-    // 使用 esbuild 优化 css 压缩
-    config.optimization
-      .minimizer('esbuild')
-      .use(ESBuildMinifyPlugin, [{ minify: true, css: true }])
+    // // 清理自带的 babel-loader
+    // rule.uses.clear()
+    //
+    // // 添加 esbuild-loader
+    // rule
+    //   .use('esbuild-loader')
+    //   .loader('esbuild-loader')
+    //   .options({
+    //     loader: 'ts', // 如果使用了 ts, 或者 vue 的 class 装饰器，则需要加上这个 option 配置， 否则会报错：ERROR: Unexpected "@"
+    //     target: 'es2015',
+    //     tsconfigRaw: require('./tsconfig.json')
+    //   })
+    // // 删除底层 terser, 换用 esbuild-minimize-plugin
+    // config.optimization.minimizers.delete('terser')
+    //
+    // // 使用 esbuild 优化 css 压缩
+    // config.optimization
+    //   .minimizer('esbuild')
+    //   .use(ESBuildMinifyPlugin, [{ minify: true, css: true }])
 
     config.resolve.symlinks(true)
     config.when(process.env.NODE_ENV === 'development', config => {
